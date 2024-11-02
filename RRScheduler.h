3aa6d85219cc5666142aa318784add4ceda95a37
Haunts06 <audrinjavier06@gmail.com>
2024-11-03 00:10:32 +0800

    RRSCHEDULER OKAY NA ATA

---
+++ RRScheduler.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "Process.h"
+#include "AScheduler.h"
+
+class RRScheduler : public AScheduler
+{
+public:
+	RRScheduler();
+	RRScheduler(int cores, int timeQuantum);
+	~RRScheduler() = default;
+	bool allProcessesFinished();
+	void init() override;
+	void execute() override;
+
+	void addProcess(std::shared_ptr<Process>, int core) override; // add process to core worker
+	void assignCore(std::shared_ptr<Process> process, int core) override; // assign core to process
+	int checkCores() override; // check if core is available
+	void printCores() override;
+private:
+	int numCores;
+	int timeQuantum;
+	std::vector<std::vector<std::shared_ptr<Process>>> processQueues;
+	std::vector<int> currentIndex;
+};
